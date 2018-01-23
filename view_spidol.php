0273b3ca1e421105562f35ea974dbc66b1c2c8dc
404NoutFound <35020209+404NoutFound@users.noreply.github.com>
2018-01-23 14:48:54 +0700

    pembetulan

--- view_spidol.php
+++ view_spidol.php
@@ -2,25 +2,27 @@
 
 class AlatTulis{
 	
-	public $warna= "hitam";
-	public $pemilik= "badrus";
+	var $warna="hitam";
+	var $pemilik="badrus";
 	
 	function Menulis(){
-		echo $this->pemilik. " menggunakan spidol warna ".$this->warna. "untuk menulis<br/>";
-	
+		return $this->pemilik." menggunakan spidol warna ".$this->warna." untuk menulis <br/>";
+	}
 	function Menggambar(){
-		echo $this->pemilik. " menggunakan spidol warna ".$this->warna. "untuk menggambar<br/>";
+		return $this->pemilik." menggunakan spidol warna ".$this->warna." untuk menggambar <br/>";
 	}
 	
 	function BukaSpidol(){
-		return $this->pemilik . " sedang membuka spidol<br/>";
+		return $this->pemilik." sedang membuka spidol <br/>";
 	}
 	
 	function TutupSpidol(){
-		return $this->pemilik . " sedang membuka spidol<br/>";
+		return $this->pemilik." sedang membuka spidol <br/>";
 	}
 }
+
 $obj_spidol = new AlatTulis();
+
 echo $obj_spidol->BukaSpidol();
 echo $obj_spidol->Menulis();
 echo $obj_spidol->Menggambar();
